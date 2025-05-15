580bbeefdf99a071d8698815e0d65892e85d4660
Victor Mendes <joaovictor_mendes@hotmail.com>
2025-05-15 18:27:10 -0300

    chore: remove initial code

--- composeApp/src/androidMain/kotlin/com/bimm/takehomeassignmnent/App.kt
+++ composeApp/src/androidMain/kotlin/com/bimm/takehomeassignmnent/App.kt
@@ -22,23 +22,6 @@ import kmptakehomeassignment.composeapp.generated.resources.compose_multiplatfor
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        AppContent()
     }
 }
