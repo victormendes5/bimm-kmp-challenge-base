3551aa762c950b4bd2a1449f6a120e96b574c4af
Victor Mendes <joaovictor_mendes@hotmail.com>
2025-05-16 20:58:44 -0300

    style: background

--- composeApp/src/commonMain/kotlin/com/bimm/takehomeassignmnent/presentation/components/ScreenHeader.kt
+++ composeApp/src/commonMain/kotlin/com/bimm/takehomeassignmnent/presentation/components/ScreenHeader.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.bimm.takehomeassignmnent.ui.theme.AppColors
 import com.bimm.takehomeassignmnent.ui.theme.Dimens
 
--- composeApp/src/commonMain/kotlin/com/bimm/takehomeassignmnent/ui/theme/AppColors.kt
+++ composeApp/src/commonMain/kotlin/com/bimm/takehomeassignmnent/ui/theme/AppColors.kt
@@ -3,7 +3,7 @@ package com.bimm.takehomeassignmnent.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
-    val Background       = Color(0xFFF5F5F5)
+    val Background       = Color.White
     val PrimaryText      = Color(0xFF212121)
     val SecondaryText    = Color(0xFF757575)
     val CardBackground   = Color(0xFFFAFAFA)
