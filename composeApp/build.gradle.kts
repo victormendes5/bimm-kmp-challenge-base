ce89afbf1bf496d8fc70fc4ad5fcca7632b6d270
Victor Mendes <joaovictor_mendes@hotmail.com>
2025-05-15 21:36:26 -0300

    feat: update theme

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
+            implementation(libs.material)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
